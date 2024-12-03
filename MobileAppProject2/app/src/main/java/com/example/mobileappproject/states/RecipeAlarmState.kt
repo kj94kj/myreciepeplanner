7ace0f8fca70c3b6dfe90be311e3a0f4e72aa5b5
kj94kj <rlwn002@naver.com>
2024-12-04 01:49:37 +0900

    no message

---
+++ MobileAppProject2/app/src/main/java/com/example/mobileappproject/states/RecipeAlarmState.kt
@@ -0,0 +1,13 @@
+package com.example.mobileappproject.states
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import java.time.LocalDate
+
+object SharedState {
+    val recipeAlarmState = MutableStateFlow(RecipeAlarmState())
+}
+
+data class RecipeAlarmState(
+    val recipeName: String="",
+    val localDate: LocalDate=LocalDate.now(),
+)
